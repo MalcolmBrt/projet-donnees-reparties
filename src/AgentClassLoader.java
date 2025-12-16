6898bbf21532a3e4ae5b2c58cc70bc1d9b8b4ba7
MojitoSensei <leo.vanhaecke609@gmail.com>
2025-12-16 09:47:08 +0100

    Update agentClassLoader & starter

--- src/AgentClassLoader.java
+++ src/AgentClassLoader.java
@@ -7,26 +7,60 @@ import java.util.Hashtable;
 
 public class AgentClassLoader extends ClassLoader {
     
-    // Chemin vers le JAR (optionnel si tout est en mémoire)
+    // Chemin vers le JAR
     private final String jarPath;
     
-    // Cache pour stocker le bytecode reçu directement (NomClasse -> byte[])
+    // Cache pour stocker le bytecode une fois lu (NomClasse -> byte[])
     private Hashtable<String, byte[]> bytecodeCache;
 
     public AgentClassLoader(String jarPath) {
-        // Délégation au chargeur système
+        // Délégation au chargeur système pour les classes Java standard
         super(AgentClassLoader.class.getClassLoader());
         this.jarPath = jarPath;
         this.bytecodeCache = new Hashtable<>();
     }
 
     /**
-     * Permet d'injecter manuellement du bytecode dans le chargeur.
-     * Utile quand l'agent arrive par le réseau et qu'on a les octets en mémoire.
+     * Cette méthode charge physiquement le bytecode depuis le fichier JAR
+     * et le stocke dans le cache.
+     * * @param className Le nom de la classe (ex: "com.monprojet.TestAgent")
+     * @return Le tableau d'octets de la classe, ou null si non trouvée/erreur.
      */
-    public void integrateCode(String className, byte[] code) {
-        this.bytecodeCache.put(className, code);
-        System.out.println("ClassLoader : Code intégré pour " + className);
+    public byte[] integrateCode(String className) {
+        // 1. Calcul du chemin interne au JAR (ex: "com/monprojet/TestAgent.class")
+        String classFile = className.replace('.', '/') + ".class";
+        
+        // 2. Construction de l'URL vers le JAR
+        File file = new File(jarPath);
+        URL url;
+        try {
+            url = new URL("jar:file:" + file.getAbsolutePath() + "!/" + classFile);
+        } catch (Exception e) {
+            System.err.println("Erreur URL JAR : " + e.getMessage());
+            return null;
+        }
+
+        // 3. Lecture du flux d'octets
+        try (InputStream is = url.openStream();
+             ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+            
+            int data;
+            while ((data = is.read()) != -1) {
+                buffer.write(data);
+            }
+            byte[] classBytes = buffer.toByteArray();
+
+            // 4. Mise en cache immédiate
+            this.bytecodeCache.put(className, classBytes);
+            System.out.println("ClassLoader : Classe " + className + " chargée depuis le JAR.");
+            
+            return classBytes;
+
+        } catch (IOException e) {
+            // La classe n'est probablement pas dans ce JAR
+            // On retourne null pour laisser findClass gérer l'erreur
+            return null;
+        }
     }
 
     /**
@@ -40,47 +74,21 @@ public class AgentClassLoader extends ClassLoader {
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         byte[] classBytes = null;
 
-        // 1. D'abord, on regarde si le code est déjà dans notre cache (via integrateCode)
+        // 1. Vérification du cache mémoire
         if (bytecodeCache.containsKey(name)) {
             classBytes = bytecodeCache.get(name);
         } 
-        // 2. Sinon, on va chercher dans le fichier JAR
-        else if (jarPath != null) {
-            try {
-                classBytes = loadBytesFromJar(name);
-                // On met en cache pour la prochaine fois
-                if (classBytes != null) {
-                    bytecodeCache.put(name, classBytes);
-                }
-            } catch (IOException e) {
-                // Ignore l'erreur ici, on lancera ClassNotFoundException à la fin si null
-            }
+        // 2. Si pas en cache, on appelle integrateCode pour aller chercher dans le JAR
+        else {
+            classBytes = integrateCode(name);
         }
 
+        // 3. Si toujours null, c'est que la classe n'existe pas
         if (classBytes == null) {
-            throw new ClassNotFoundException("Classe introuvable (ni cache, ni JAR) : " + name);
+            throw new ClassNotFoundException("Classe introuvable dans le JAR : " + name);
         }
 
-        // 3. Transformation des octets en Classe Java
+        // 4. Transformation des octets en Classe Java
         return defineClass(name, classBytes, 0, classBytes.length);
-    }
-
-    /**
-     * Méthode utilitaire privée pour extraire les octets du JAR.
-     */
-    private byte[] loadBytesFromJar(String name) throws IOException {
-        String classFile = name.replace('.', '/') + ".class";
-        File file = new File(jarPath);
-        URL url = new URL("jar:file:" + file.getAbsolutePath() + "!/" + classFile);
-
-        try (InputStream is = url.openStream();
-             ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
-            
-            int data;
-            while ((data = is.read()) != -1) {
-                buffer.write(data);
-            }
-            return buffer.toByteArray();
-        }
     }
 }
--- src/Starter.java
+++ src/Starter.java
@@ -8,37 +8,17 @@ public class Starter {
         this.loader = loader;
     }
 
-    /**
-     * Crée une nouvelle instance d'une classe en utilisant le ClassLoader associé.
-     * Cette méthode utilise la réflexion Java.
-     * * @param className Le nom complet de la classe à instancier (ex: "TestAgent")
-     * @param args Les arguments à passer au constructeur (peut être vide)
-     * @return L'objet instancié (Object)
-     */
     public Object newInstance(String className, Object... args) throws Exception {
-        // 1. Chargement de la classe via notre ClassLoader personnalisé
-        // Cela va déclencher la méthode findClass() du AgentClassLoader
+        // Appelera findClass -> integrateCode -> lecture du JAR
         Class<?> clazz = loader.loadClass(className);
 
-        // 2. Recherche du constructeur approprié
-        // Pour simplifier, on cherche ici un constructeur qui correspond aux types des arguments passés
-        Class<?>[] paramTypes = new Class[args.length];
-        for (int i = 0; i < args.length; i++) {
-            paramTypes[i] = args[i].getClass();
-            // Note : La gestion des types primitifs ou interfaces peut nécessiter plus de logique
-            // Si vous utilisez TestAgent(Queue, String), assurez-vous de passer les bons types ici.
-        }
-
-        // Si aucun argument n'est passé, on utilise le constructeur par défaut
-        if (args.length == 0) {
-            return clazz.newInstance();
-        } else {
-            // Recherche du constructeur spécifique
-            // NOTE : C'est une simplification. Dans votre projet réel, comme TestAgent
-            // prend (Queue, String), il faudra peut-être hardcoder les types ici ou 
-            // parcourir les constructeurs disponibles.
-            Constructor<?> constructor = clazz.getConstructors()[0]; // Prend le 1er constructeur public dispo
+        // Recherche simple du premier constructeur disponible
+        // (Pour une vraie app, il faudrait chercher le constructeur correspondant aux types des args)
+        if (args.length > 0) {
+            Constructor<?> constructor = clazz.getConstructors()[0];
             return constructor.newInstance(args);
+        } else {
+            return clazz.newInstance();
         }
     }
 }
